8bc7da641c5a994752fc9a9b39ca04479f88ed0b
AnhHuy <90122214+anhhuy007@users.noreply.github.com>
2023-04-21 23:18:36 +0700

    Delete Game.h

--- CaroGame/Game.h
+++
@@ -1,7 +0,0 @@
-void gotoXY(int x, int y);
-void drawBoard();
-void resetGame();
-void fixConsoleWindow();
-void startNewGame();
-void playGame(bool isFirstPlayerTurn, int**& board);
-bool isValidPosition(int x, int y, int i, int j, int**& board);
